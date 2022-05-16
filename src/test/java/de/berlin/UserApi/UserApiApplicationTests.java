cf04a6aa3323673858a5795d7215afb42b1a3302
OliKue# <oliver-kuetemeier123@web.de>
2022-05-16 08:53:56 +0200

    project setup via start.spring.io

---
+++ src/test/java/de/berlin/UserApi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.berlin.UserApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
