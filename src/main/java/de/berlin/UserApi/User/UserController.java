a62be571493653fc759d8eb7a0918a156b1757d6
OliKue# <oliver-kuetemeier123@web.de>
2022-05-16 09:19:24 +0200

    add UserController

---
+++ src/main/java/de/berlin/UserApi/User/UserController.java
@@ -0,0 +1,9 @@
+package de.berlin.UserApi.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class UserController {
+}
