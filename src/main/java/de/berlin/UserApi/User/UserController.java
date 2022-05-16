3982213f231bc8a79d9c33fd56ffa00986f5cb8e
OliKue# <oliver-kuetemeier123@web.de>
2022-05-16 12:34:50 +0200

    change Responetype

--- src/main/java/de/berlin/UserApi/User/UserController.java
+++ src/main/java/de/berlin/UserApi/User/UserController.java
@@ -1,6 +1,8 @@
 package de.berlin.UserApi.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,33 +13,38 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/user")
-    public User saveUser(@RequestBody User user){
-        return userService.save(user);
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/user/{id}")
-    public User getUser(@PathVariable("id") long id) {
-        return userService.findUserById(id);
+    public ResponseEntity getUser(@PathVariable("id") long id) {
+        try{
+            return new ResponseEntity<User>(userService.findUserById(id), HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.NO_CONTENT);
+        }
     }
 
     @GetMapping(value = "/user")
-    public List<User> getUsers() {
-        return userService.getAllUsers();
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/user/byFirstName/{firstName}")
-    public List<User> getUsersWithFirstName(@PathVariable("firstName") String firstName) {
-        return userService.getAllUserByFirstName(firstName);
+    public ResponseEntity<List<User>> getUsersWithFirstName(@PathVariable("firstName") String firstName) {
+        return new ResponseEntity<>(userService.getAllUserByFirstName(firstName), HttpStatus.OK);
     }
 
     @PutMapping(value = "/user/{id}")
-    public User updateUser(@PathVariable("id")  long id, @RequestBody User user){
-        return userService.saveOrUpdate(user, id);
+    public ResponseEntity<User> updateUser(@PathVariable("id") long id, @RequestBody User user) {
+        return new ResponseEntity<>(userService.saveOrUpdate(user, id),HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/user/{id}")
-    public void deleteUser(@PathVariable("id")  long id){
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) {
         userService.deleteUser(id);
+        return new ResponseEntity("User deleted.", HttpStatus.OK);
     }
 
 }
